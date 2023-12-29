15338114e9ab933a77a3829b9b6846f3186fe743
sethdanny <naddulidaniel1994@gmail.com>
2023-12-30 02:07:41 +0300

    generate token so sign in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,10 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import User from '../models/userModel.js';
 
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: '1d'})
+}
+
 export const registerUser = asyncHandler(
     async (req, res) => {
         const {name, email, password} = req.body;
@@ -16,10 +20,27 @@ export const registerUser = asyncHandler(
             throw new Error('password must be up to 6 characters')
         }
         // check if a user exists
-        const userExists = await User.find({email});
+        const userExists = await User.findOne({email});
         if (userExists) {
             throw new Error('Email already been registered');
         }
-
-
+        const user = await User.create({
+            name,
+            email,
+            password
+        })
+        //generate token 
+        const token = generateToken(user._id);
+        if(user) {
+            res.cookie("token", token, {
+                path: '/',
+                httpOnly: true,
+                expiresIn: new Date(Date.now() + 1000 * 86400),
+                secure: true,
+                samesite: none
+            })
+        } else {
+            res.status(400)
+            throw new Error('Invalid user data')
+        }
     });
